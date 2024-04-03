475dfc1aadf54d9685692fccf6c0825964cbb1d8
ldmcgo26 <ldmcgo26@colby.edu>
2024-04-02 21:23:23 -0400

    player

---
+++ PlayerAlgorithm.py
@@ -0,0 +1,10 @@
+from Graph import Graph
+
+class PlayerAlgorithm:
+
+    def __init__(self, graph=Graph(), values={}):
+        self.graph = graph
+        self.values = values
+    
+    def makeMove(self, ):
+        pass
