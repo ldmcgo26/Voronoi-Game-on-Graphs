707c22eb8ee17d3a5d7926c8a80259b679645891
ldmcgo26 <ldmcgo26@colby.edu>
2024-04-05 13:58:25 -0400

    fixing stuff

--- PlayerAlgorithm.py
+++ PlayerAlgorithm.py
@@ -14,7 +14,7 @@ class PlayerAlgorithm:
         #list of the players, which gives us each players controlled vertices and their value assessments
         self.players = players
     
-    #This should add an untaken vertex to one of the player's list of controlled vertices.
+    #This should make an untaken vertex one of the player's facilities.
     #QUESTION: Should this check whether or not the vertex is not already controlled by another player?
     #Should this do anything else?
     def makeMove(self, vertex: int, player: Player):
